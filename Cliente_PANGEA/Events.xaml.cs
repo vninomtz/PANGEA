7b7669348b9c01d3c6699167ed924667343278f9
AlanGlezH <47785262+AlanGlezH@users.noreply.github.com>
2020-03-09 18:12:59 -0600

    GUI de CU-01 y CU02

---
+++ Cliente_PANGEA/Events.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cliente_PANGEA
+{
+    /// <summary>
+    /// Interaction logic for Events.xaml
+    /// </summary>
+    public partial class Events : Window
+    {
+        public Events()
+        {
+            InitializeComponent();
+            centralFrame.Navigate(new ShowEvents());
+            centralFrame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+            Button_back.Visibility = Visibility.Hidden;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            centralFrame.Navigate(new NewEvent());
+            Button_back.Visibility = Visibility.Visible;
+            Button_add.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_back_Click(object sender, RoutedEventArgs e)
+        {
+            Button_add.Visibility = Visibility.Visible;
+            Button_back.Visibility = Visibility.Hidden;
+
+            centralFrame.Navigate(new ShowEvents());
+
+        }
+    }
+}

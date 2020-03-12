31f9ed8cea7122b188767def26eeb608a19cc646
Victor Niño <ninomtz.victor@gmail.com>
2020-03-11 21:36:07 -0600

    CU05 guardar comité en la BD

--- Cliente_PANGEA/Views/CrearComite.xaml.cs
+++ Cliente_PANGEA/Views/CrearComite.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using DataAccess;
+using Cliente_PANGEA.Controllers;
 
 namespace Cliente_PANGEA.Views
 {
@@ -23,6 +26,79 @@ namespace Cliente_PANGEA.Views
         public CrearComite()
         {
             InitializeComponent();
+        }
+
+        public void ShowMessageError(string type, string message)
+        {
+            MessageBox.Show($"{type}: por favor corregir el campo {message}");
+        }
+        public bool EmptyFields()
+        {
+            string errorType = "Campos vacíos";
+            bool result = false;
+            if (string.IsNullOrEmpty(txt_nombreComite.Text))
+            {
+                ShowMessageError(errorType, "Nombre de comité");
+                result = true;
+            }
+            if (string.IsNullOrEmpty(txt_descripcionComite.Text))
+            {
+                ShowMessageError(errorType, "Descripción de comité");
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool CorrectFields()
+        {
+            string typeError = "Campo inválido";
+            bool result = true;
+            Regex regexName = new Regex(@"^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ0-9 ]+$");
+            if (!regexName.IsMatch(txt_nombreComite.Text))
+            {
+                ShowMessageError(typeError, "Nombre comité");
+                result = false;
+            }
+            Regex regexDescription = new Regex(@"^[\r\n a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ0-9 !@#\$%\&\*\?¿._~\/]+$");
+            if (!regexDescription.IsMatch(txt_descripcionComite.Text))
+            {
+                ShowMessageError(typeError, "Descripción comité");
+                result = false;
+            }
+            return result;
+        }
+
+       
+
+
+        private void btn_guardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!EmptyFields())
+            {
+                if (CorrectFields())
+                {
+                    if (!ComiteController.ExistingCommittee(txt_nombreComite.Text))
+                    {
+                        DataAccess.Comites comite = new Comites
+                        {
+                            Nombre = txt_nombreComite.Text,
+                            Descripcion = txt_descripcionComite.Text,
+                            FechaCreacion = DateTime.Now
+                        };
+                        int result = ComiteController.SaveCommittee(comite);
+                        if(result > 0)
+                        {
+                            MessageBox.Show("Se guardo con éxito el Comité");
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al guardar el comité");
+                        }
+                    }
+                }
+            }
         }
     }
 }

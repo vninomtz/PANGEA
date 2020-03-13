161254b86871caed9b6c737524a0ba30f24403dc
Victor Niño <ninomtz.victor@gmail.com>
2020-03-13 16:40:21 -0600

    Pruebas de crear comité

---
+++ UnitTest/ComiteTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cliente_PANGEA.Controllers;
+using Cliente_PANGEA.Views;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ComiteTest
+    {
+        
+        [TestMethod]
+        public void ExistingCommitteeTest()
+        {
+            bool expected = true;
+            string name = "Administración de conferencias magistrales";
+            bool result = ComiteController.ExistingCommittee(name);
+            Assert.AreEqual(expected, result);
+            
+        }
+
+        [TestMethod]
+        public void NotExistingComitteeTest()
+        {
+            bool expected = false;
+            string name = "Comité no existente";
+            bool result = ComiteController.ExistingCommittee(name);
+            Assert.AreEqual(expected, result);
+        }
+
+        
+
+
+    }
+}

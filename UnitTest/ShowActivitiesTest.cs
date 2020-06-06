2c8079e2352f51a144ad5346360d2e024d5530de
IvanGutru <47703419+IvanGutru@users.noreply.github.com>
2020-06-06 04:27:59 -0500

    Conclusión de CU46
    Construcción de CU46 con sus respectivos artefactos y pruebas unitarias

---
+++ UnitTest/ShowActivitiesTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Cliente_PANGEA.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ShowActivitiesTest
+    {
+        List<Actividades> activityList = new List<Actividades>();
+        [TestMethod]
+        public void ShowActivities()
+        {
+            int idEvent = 1;
+            activityList = ActivityController.GetEventActivities(idEvent);
+            Assert.IsNotNull(activityList);
+        }
+    }
+}
